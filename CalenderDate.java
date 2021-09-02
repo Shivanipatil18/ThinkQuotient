30604a2c22adad31e34c922bcf4077b141f226d8
Shivanipatil18 <89718110+Shivanipatil18@users.noreply.github.com>
2021-09-02 01:56:43 -0700

    Add files via upload

---
+++ CalenderDate.java
@@ -0,0 +1,70 @@
+package date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class CalenderDate {
+
+	static WebDriver driver;
+	 public static void main(String[] args) throws InterruptedException
+	 {
+		 System.setProperty("webdriver.chrome.driver","E:\\worksoft\\chromedriver_win32\\chromedriver.exe");
+		 driver=new ChromeDriver();
+		 driver.get("https://jqueryui.com/resources/demos/datepicker/other-months.html");
+		 driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+		 driver.manage().window().maximize();
+		 driver.findElement(By.id("datepicker")).click();
+		 
+		 
+		 WebElement yymm=driver.findElement(By.cssSelector(".ui-datepicker-title"));
+		 yymm.click();
+		 System.out.println("YYMM is "+ yymm.getText());
+		 
+		 selectDateInCalender("12/06/2020");
+	 }
+		 
+	public static void selectDateInCalender(String date) throws InterruptedException {
+
+		 Date currentDate= new Date();
+		 SimpleDateFormat dateFormat= new SimpleDateFormat("dd/MM/yyyy");
+		 try
+		 {
+
+			Date expectedDate = dateFormat.parse(date);
+			 String day = new SimpleDateFormat("dd").format(expectedDate);
+			 String month = new SimpleDateFormat("MMMM").format(expectedDate);
+			 String year = new SimpleDateFormat("yyyy").format(expectedDate);
+			 String expectedMonthYear = month + " " + year;
+			 while(true)
+			 {
+				 String displayDate = driver.findElement(By.cssSelector(".ui-datepicker-calendar")).getText();
+				 if(expectedMonthYear.equals(displayDate))
+				 {
+					 driver.findElement(By.xpath("//a[text()= '" + day + "']")).click();
+					 break;
+				 }
+				 else if(expectedDate.compareTo(currentDate)>0)
+				 {
+					 driver.findElement(By.xpath("//span[text()='Next']")).click();
+					 Thread.sleep(1000);
+				 }
+				 else {
+					 driver.findElement(By.xpath("//span[text()='Prev']")).click();
+					 Thread.sleep(1000);
+				 }
+			 }
+		 }
+		 catch(ParseException e){
+			 e.printStackTrace();
+		 }
+	 }
+	 
+}
